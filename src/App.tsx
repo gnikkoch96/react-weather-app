854a1cac5c26d9962450e11f3598385efd5c0df6
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-04 22:11:59 -0700

    feat: created the useWeatherApiService() hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { useWeatherAPIService } from "./hooks/useWeatherAPIService.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const weatherApiService = useWeatherAPIService();
 
   return (
     <>
-     
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hooks/useWeatherAPIService.tsx
+++ src/hooks/useWeatherAPIService.tsx
@@ -4,7 +4,31 @@ type LocationData = {
     location: string
 }
 
-export function useWeatherAPIService(locationData: LocationData){
+type WeatherData = {
+    
+}
+
+async function callWeatherAPI(url: string){
+    try{
+        const response = await fetch(url);
+        
+        if(!response.ok){
+            throw new Error(`Response Stats: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+
+    }catch(error: any){
+        console.log(error.message);
+    }
+}
+
+export async function useWeatherAPIService(locationData?: LocationData){
     const [weatherData, setWeatherData] = useState();
 
+    const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,is_day,wind_speed_10m&timezone=America%2FLos_Angeles'
+
+
+    // await callWeatherAPI(url);
 }
