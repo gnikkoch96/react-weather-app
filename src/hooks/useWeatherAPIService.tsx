86fd0f4fae799d83f2a4d2d74b2d0d4f2908e559
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-07 20:52:57 -0700

    feat: finished settingup weather api service hook

--- src/hooks/useWeatherAPIService.tsx
+++ src/hooks/useWeatherAPIService.tsx
@@ -1,34 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type LocationData = {
-    location: string
-}
+  location: string;
+};
 
 type WeatherData = {
-    
-}
+  interval: number;
+  is_day: number;
+  relative_humidity: number;
+  temperature: number;
+  time: number;
+  wind_speed: number;
+};
 
-async function callWeatherAPI(url: string){
-    try{
-        const response = await fetch(url);
-        
-        if(!response.ok){
-            throw new Error(`Response Stats: ${response.status}`);
-        }
+async function callWeatherAPI(url: string) {
+  try {
+    const response = await fetch(url);
 
-        const result = await response.json();
-        console.log(result);
-
-    }catch(error: any){
-        console.log(error.message);
+    if (!response.ok) {
+      throw new Error(`Response Stats: ${response.status}`);
     }
+
+    const result = await response.json();
+    return result;
+  } catch (error: any) {
+    console.log(error.message);
+  }
+
+  return null;
 }
 
-export async function useWeatherAPIService(locationData?: LocationData){
-    const [weatherData, setWeatherData] = useState();
+export function useWeatherAPIService(locationData?: LocationData) {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
 
-    const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,is_day,wind_speed_10m&timezone=America%2FLos_Angeles'
+  useEffect(() => {
+    const url =
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,is_day,wind_speed_10m&timezone=America%2FLos_Angeles";
 
+    const fetchWeatherData = async () => {
+      const data = await callWeatherAPI(url);
 
-    // await callWeatherAPI(url);
+      const wData: WeatherData = {
+        time: data.current.time,
+        interval: data.current.interval,
+        temperature: data.current.temperature_2m,
+        relative_humidity: data.current.relative_humidity_2m,
+        is_day: data.current.is_day,
+        wind_speed: data.current.wind_speed_10m   
+      }
+
+      setWeatherData(wData);
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  return weatherData;
 }
