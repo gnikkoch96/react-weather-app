3a1bd9ac9bf006d9af3ec903036e29596f017ac2
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-07 20:54:25 -0700

    chore: removed unnecessary count state var

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useWeatherAPIService } from "./hooks/useWeatherAPIService.js";
 
 function App() {
-  const [count, setCount] = useState(0);
   const weatherData = useWeatherAPIService();
 
   return (
