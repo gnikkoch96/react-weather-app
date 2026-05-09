84948a5ddb9176354102dbf3466bf5c0b5c3f019
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-09 16:40:44 -0700

    feat: add lucide react library

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 import { useWeatherAPIService } from "./hooks/useWeatherAPIService.js";
 
 function App() {
-    const {error, isLoading, weatherData} = useWeatherAPIService();
-
+  const { error, isLoading, weatherData } = useWeatherAPIService();
   return (
     <>
-      
+      <p>
+        {isLoading ? (
+          "Loading..."
+        ) : weatherData ? (
+          <div>
+            <p>{weatherData.interval}</p>
+            <p>{weatherData.is_day}</p>
+            <p>{weatherData.relative_humidity}</p>
+            <p>{weatherData.temperature}</p>
+            <p>{weatherData.time}</p>
+            <p>{weatherData.wind_speed}</p>
+          </div>
+        ) : (
+          error
+        )}
+      </p>
     </>
   );
 }
