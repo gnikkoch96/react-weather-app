c1ed8a7c7acfd34dae0eae6e86157f975ed765f2
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-11 21:53:42 -0700

    style: centered weather card

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useWeatherAPIService } from "./hooks/useWeatherAPIService.js";
 function App() {
   const { error, isLoading, weatherData } = useWeatherAPIService();
   return (
-    <div className="min-h-screen bg-blue-400">
+    <div className="min-h-screen flex justify-center items-center bg-blue-400">
       {isLoading ? (
         "Loading..."
       ) : weatherData ? (
