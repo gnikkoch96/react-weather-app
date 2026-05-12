3e820ef2690a1b0bf2145bd34272dc75e564c61b
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-12 09:48:58 -0700

    style: added padding, rounded borders, and shadows to WeatherCard

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -2,7 +2,7 @@ import type { WeatherData } from "../../types/weather/types.js"
 
 export default function WeatherCard({weatherData} : {weatherData: WeatherData}){
     return (
-        <div className="">
+        <div className="p-4 rounded shadow-sm bg-blue-300">
             <p>{weatherData.temperature}</p>
             <p>{weatherData.time}</p>
             <p>{weatherData.relative_humidity}</p>
