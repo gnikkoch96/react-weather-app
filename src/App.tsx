0f60d8ba0937dc78ba9ab4a282200cb226afc951
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-12 09:41:15 -0700

    style: made background gradient of blue

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useWeatherAPIService } from "./hooks/useWeatherAPIService.js";
 function App() {
   const { error, isLoading, weatherData } = useWeatherAPIService();
   return (
-    <div className="min-h-screen flex justify-center items-center bg-blue-400">
+    <div className="min-h-screen flex justify-center items-center bg-linear-to-r from-blue-900 to-blue-600">
       {isLoading ? (
         "Loading..."
       ) : weatherData ? (
