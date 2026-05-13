a6c3f69fef3160a76fed261de525bb253ac01a6e
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-13 11:18:49 -0700

    style: updated p tags to div and made it flex surrounding the relative humidity and wind speed also added icons

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,13 +1,26 @@
-import type { WeatherData } from "../../types/weather/types.js"
+import type { WeatherData } from "../../types/weather/types.js";
+import { createLucideIcon, Droplet, Wind } from "lucide-react";
 
-export default function WeatherCard({weatherData} : {weatherData: WeatherData}){
-    return (
-        <div className="p-4 rounded shadow-sm bg-blue-300">
-            <p>{weatherData.temperature}</p>
-            <p>{weatherData.time}</p>
-            <p>{weatherData.relative_humidity}</p>
-            <p>{weatherData.wind_speed}</p>
+export default function WeatherCard({
+  weatherData,
+}: {
+  weatherData: WeatherData;
+}) {
+  return (
+    <div className="max-w-md flex flex-col justify-between p-4 rounded shadow-sm bg-blue-300">
+      <p>{weatherData.time}</p>
+      {/* todo insert icon */}
+      <p className="text-6xl">{weatherData.temperature} C</p>
+      <div className="flex justify-between text-2xl">
+        <div className="flex justify-center items-center">
+          <Droplet /> 
+          Relative Humidity {weatherData.relative_humidity}
         </div>
-    )
+        <div className="flex justify-center items-center">
+          <Wind /> 
+          Wind Speed {weatherData.wind_speed}
+        </div>
+      </div>
+    </div>
+  );
 }
-
