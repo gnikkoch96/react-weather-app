0419189e3994c89f79dee0b9b67728e9b44b23b9
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-14 17:25:52 -0700

    feat: created a module-level weather code to icon record to map weather code to lucid icon

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -16,6 +16,41 @@ import {
   ThermometerSnowflake,
 } from "lucide-react";
 
+const WEATHER_CODE_TO_ICON: Record<number, React.ReactNode> = {
+  0: <Sun/>,
+  1: <CloudSun/>,
+  2: <CloudSun/>,
+  3: <Cloud/>,
+  45: <CloudFog />,
+  48: <CloudFog />,
+  51: <CloudDrizzle />,
+  53: <CloudDrizzle />,
+  55: <CloudDrizzle />,
+  56: <CloudHail />,
+  57: <CloudHail />,
+  61: <CloudRain />,
+  63: <CloudRain />,
+  65: <CloudRain />,
+  66: <ThermometerSnowflake />,
+  67: <ThermometerSnowflake />,
+  71: <Snowflake />,
+  73: <Snowflake />,
+  75: <Snowflake />,
+  77: <CloudSnow />,
+  80: <CloudRainWind />,
+  81: <CloudRainWind />,
+  82: <CloudRainWind />,
+  85: <CloudSnow />,
+  86: <CloudSnow />,
+  95: <CloudLightning />,
+  96: <CloudLightning />,
+  99: <CloudLightning />,
+}
+
+function loadWeatherIcon(weatherCode: number){
+    
+}
+
 export default function WeatherCard({
   weatherData,
 }: {
@@ -24,8 +59,10 @@ export default function WeatherCard({
   return (
     <div className="max-w-md flex flex-col justify-between p-4 rounded shadow-sm bg-blue-300">
       <p>{weatherData.time}</p>
-      {/* todo insert icon */}
-      <p className="text-6xl">{weatherData.temperature} C</p>
+      <div>
+
+        <p className="text-6xl">{weatherData.temperature} C</p>
+      </div>
       <div className="flex justify-between text-2xl">
         <div className="flex justify-center items-center">
           <Droplet />
