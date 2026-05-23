093c2bcf18bc72ab2faba637276b27fce6d6b37a
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-23 09:43:56 -0700

    feat: created a weather slice for redux

---
+++ src/redux/weatherSlice.ts
@@ -0,0 +1,26 @@
+import {createSlice, type PayloadAction} from '@reduxjs/toolkit';
+
+// intialize the state for the slice
+const initialState = {
+    temperatureUnit: 'fahrenheit', // celcius is default in the api
+    speedUnit: '' // kmh is by default in the api
+}
+
+const weatherSlice = createSlice({
+    name: 'weather_config',
+    initialState, 
+
+    reducers: {
+        setTemperatureUnit: (state, action: PayloadAction<any>) => {
+            state.temperatureUnit = action.payload;
+        },
+
+        setSpeedUnit: (state, action: PayloadAction<any>) => {
+            state.speedUnit = action.payload
+        }
+    }
+})
+
+
+export const {setTemperatureUnit, setSpeedUnit} = weatherSlice.actions;
+export default weatherSlice.reducer;
