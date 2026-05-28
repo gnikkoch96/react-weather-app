620a767c8d06f4b7f0322755e367d0def0fe1e8c
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-28 09:50:44 -0700

    feat: nested settings into another div to represent the card while the parent div is the backdrop

--- src/components/SettingsPopup.tsx
+++ src/components/SettingsPopup.tsx
@@ -1,33 +1,36 @@
 export default function SettingsPopup({ className }: { className?: string }) {
   return (
-    <div>
-      <div>
-        {/* Temperature */}
-        <label>
-          Temperature Unit:
-          <select name="temperature-unit" id="">
-            <option value={"farenheit"}>Farenheit</option>
-            <option value={"celcius"}>Celcius</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        {/* Speed */}
-        <label>
-          Speed Unit:
-          <select name="speed-unit" id="">
-            <option value={"kmh"}>km/h</option>
-            <option value={"ms"}>m/s</option>
-            <option value={"mph"}>mph</option>
-            <option value={"kn"}>Knots</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        {/* Save or Exit */}
-        <button>Exit</button>
-        <button>Save</button>
-      </div>
+    <div className='absolute min-w-screen min-h-screen flex justify-center items-center bg-gray-400 '>
+        {/* Settings Card */}
+        <div className="bg-linear-to-r from-blue-600 to-blue-400 text-white">
+            <div>
+                {/* Temperature */}
+                <label>
+                Temperature Unit:
+                <select name="temperature-unit" id="">
+                    <option value={"farenheit"}>Farenheit</option>
+                    <option value={"celcius"}>Celcius</option>
+                </select>
+                </label>
+            </div>
+            <div>
+                {/* Speed */}
+                <label>
+                Speed Unit:
+                <select name="speed-unit" id="">
+                    <option value={"kmh"}>km/h</option>
+                    <option value={"ms"}>m/s</option>
+                    <option value={"mph"}>mph</option>
+                    <option value={"kn"}>Knots</option>
+                </select>
+                </label>
+            </div>
+            <div>
+                {/* Save or Exit */}
+                <button>Exit</button>
+                <button>Save</button>
+            </div>
+        </div>
     </div>
   );
 }
