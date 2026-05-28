22bb8f783579ba0872f7da4438a801953a82ac6a
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-28 09:36:26 -0700

    feat: created settings popup frame and added selectors and options for temperature unit and speed unit

--- src/components/SettingsPopup.tsx
+++ src/components/SettingsPopup.tsx
@@ -1,5 +1,29 @@
-export default function SettingsPopup({className}: {className?: string}){
-    return(
-        <div></div>
-    )
+export default function SettingsPopup({ className }: { className?: string }) {
+  return (
+    <div>
+      <div>
+        {/* Temperature */}
+        <label>
+          Temperature Unit:
+          <select name="temperature-unit" id="">
+            <option value={"farenheit"}>Farenheit</option>
+            <option value={"celcius"}>Celcius</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        {/* Speed */}
+        <label>
+          Speed Unit:
+          <select name="speed-unit" id="">
+            <option value={"kmh"}>km/h</option>
+            <option value={"ms"}>m/s</option>
+            <option value={"mph"}>mph</option>
+            <option value={"kn"}>Knots</option>
+          </select>
+        </label>
+      </div>
+      <div>{/* Save or Exit */}</div>
+    </div>
+  );
 }
