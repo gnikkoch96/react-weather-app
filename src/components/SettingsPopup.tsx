3dbfad1d9072de134c7b702f10bb248993328eed
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-30 09:21:04 -0700

    style: turned pop card to blue-400 only (no more gradient), added a X button (aligned to the right), aligned select more to the right for both temperature unit and speed unit via flex grow.

--- src/components/SettingsPopup.tsx
+++ src/components/SettingsPopup.tsx
@@ -1,27 +1,31 @@
+import { X } from "lucide-react";
+
 export default function SettingsPopup({ className }: { className?: string }) {
   return (
-    <div className="absolute min-w-screen min-h-screen flex justify-center items-center transparent backdrop-blur-xs">
+    <div className="absolute min-w-screen min-h-screen flex justify-center items-center bg-transparent backdrop-blur-xs">
       {/* Settings Card */}
-      <div className="text-2xl bg-linear-to-r from-blue-600 to-blue-400 text-white">
+      <div className="p-4 text-2xl shadow-2xl rounded border bg-blue-400 text-white">
+        <button className="cursor-pointer w-full flex justify-end "><X size={16}/></button>
+
         {/* Temperature */}
         <label className="flex gap-2">
-          Temperature Unit:
-          <select name="temperature-unit" id="">
+          <span className="flex-1/2">Temperature Unit:</span>
+          <select className="bg-blue-400" name="temperature-unit" id="">
             <option value={"farenheit"}>Farenheit</option>
             <option value={"celcius"}>Celcius</option>
           </select>
         </label>
         {/* Speed */}
-        <label className="flex gap-2">
-          Speed Unit:
-          <select name="speed-unit" id="">
+        <label className="flex">
+          <span className="flex-1/4">Speed Unit:</span>
+          <select className="bg-blue-400" name="speed-unit" id="">
             <option value={"kmh"}>km/h</option>
             <option value={"ms"}>m/s</option>
             <option value={"mph"}>mph</option>
             <option value={"kn"}>Knots</option>
           </select>
         </label>
-        <div className="flex gap-2">
+        <div className="flex justify-between">
           {/* Save or Exit */}
           <button>Exit</button>
           <button>Save</button>
