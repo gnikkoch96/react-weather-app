a3ad58e0d3c2aa7dee32c8223f8e9e49ae08ae6b
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-31 10:01:35 -0700

    feat: app now listens to the isVisible state froms setting slice and passes the state down to the settings popup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import SettingsButton from "./components/SettingsButton.js";
 import SettingsPopup from "./components/SettingsPopup.js";
 import WeatherCard from "./components/WeatherCard.js";
 import { useWeatherAPIService } from "./hooks/useWeatherAPIService.js";
+import { useAppSelector } from "./hooks/useAppSelector.js";
 
 function App() {
   const { error, isLoading, weatherData } = useWeatherAPIService();
+  const isSettingsVisible = useAppSelector((state) => state.settingsConfig.isVisible);
+
   return (
     <div className="min-h-screen flex justify-center items-center bg-blue-800">
       <SettingsButton/>
-      <SettingsPopup/>
+      <SettingsPopup isVisible={isSettingsVisible}/>
       {isLoading ? (
         "Loading..."
       ) : weatherData ? (
