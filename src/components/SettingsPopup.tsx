12eeb2d743d8343af9c7fbb5ff63c3c15496cecb
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-05-31 09:47:45 -0700

    style: changed bg to white, action button colors, changed x size to 24, changed border color for the selector to represent more off-white color

--- src/components/SettingsPopup.tsx
+++ src/components/SettingsPopup.tsx
@@ -4,13 +4,22 @@ export default function SettingsPopup({ className }: { className?: string }) {
   return (
     <div className="absolute min-w-screen min-h-screen flex justify-center items-center bg-transparent backdrop-blur-xs">
       {/* Settings Card */}
-      <div className="flex flex-col gap-4 p-4 text-2xl shadow-2xl rounded border bg-blue-400 text-white">
-        <button className="cursor-pointer  w-full flex justify-end"><X className="border rounded" size={16}/></button>
+      <div className="flex flex-col gap-4 p-4 text-2xl shadow-2xl rounded border bg-white">
+        <div className="w-full flex justify-between">
+          <span>Settings</span>
+          <button className="cursor-pointer">
+            <X className="" size={24} />
+          </button>
+        </div>
 
         {/* Temperature */}
         <label className="flex gap-2">
           <span className="flex-1/2">Temperature Unit:</span>
-          <select className="border rounded bg-blue-400" name="temperature-unit" id="">
+          <select
+            className="border px-1 border-gray-500 rounded"
+            name="temperature-unit"
+            id=""
+          >
             <option value={"farenheit"}>Farenheit</option>
             <option value={"celcius"}>Celcius</option>
           </select>
@@ -18,17 +27,25 @@ export default function SettingsPopup({ className }: { className?: string }) {
         {/* Speed */}
         <label className="flex">
           <span className="flex-1/4">Speed Unit:</span>
-          <select className="border rounded bg-blue-400" name="speed-unit" id="">
+          <select
+            className="border px-1 border-gray-300 rounded"
+            name="speed-unit"
+            id=""
+          >
             <option value={"kmh"}>km/h</option>
             <option value={"ms"}>m/s</option>
             <option value={"mph"}>mph</option>
             <option value={"kn"}>Knots</option>
           </select>
         </label>
-        <div className="flex justify-around">
+        <div className="flex justify-around gap-1">
           {/* Save or Exit */}
-          <button className="cursor-pointer rounded border px-4 shadow bg-red-400">Exit</button>
-          <button className="cursor-pointer rounded border px-4 shadow bg-green-400">Save</button>
+          <button className="cursor-pointer rounded px-4 flex-1 bg-gray-300">
+            Exit
+          </button>
+          <button className="cursor-pointer rounded px-4 flex-1 bg-linear-to-r from-blue-600 to-blue-400 text-white">
+            Save
+          </button>
         </div>
       </div>
     </div>
