b22085903774e16e9e6464b11ed2c294dc456ff6
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-01 10:03:03 -0700

    feat: toggles the visiblity of the popup in the settings global state

--- src/components/SettingsButton.tsx
+++ src/components/SettingsButton.tsx
@@ -1,9 +1,14 @@
 import { Settings } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { setIsVisible } from "../redux/settingsSlice.js";
 
 export default function SettingsButton({ className }: { className?: string }) {
+  const dispatch = useDispatch();
+
   return (
     <button
       className={`fixed top-2 left-2 cursor-pointer text-white ${className}`}
+      onClick={() => dispatch(setIsVisible(true))}
     >
       <Settings size={32} />
     </button>
