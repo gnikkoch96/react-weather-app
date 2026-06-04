8e577129b17cb0f51d72b3ebe81c2c98b96df634
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-04 16:02:32 -0700

    feat: added typing to the initial state so that I can initialize the value properly in the SettingsPopup.tsx

--- src/redux/weatherSlice.ts
+++ src/redux/weatherSlice.ts
@@ -1,27 +1,24 @@
-import {createSlice, type PayloadAction} from '@reduxjs/toolkit';
-import type { TemperatureUnit, SpeedUnit } from '../../types/weather/types.js';
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { TemperatureUnit, SpeedUnit } from "../../types/weather/types.js";
 
 // intialize the state for the slice
-const initialState = {
-    temperatureUnit: 'fahrenheit', // celcius is default in the api
-    speedUnit: 'kmh' // kmh is by default in the api
-}
+const initialState: { temperatureUnit: TemperatureUnit; speedUnit: SpeedUnit } =
+  { temperatureUnit: "fahrenheit", speedUnit: "kmh" };
 
 const weatherSlice = createSlice({
-    name: 'weatherConfig',
-    initialState, 
+  name: "weatherConfig",
+  initialState,
 
-    reducers: {
-        setTemperatureUnit: (state, action: PayloadAction<TemperatureUnit>) => {
-            state.temperatureUnit = action.payload;
-        },
+  reducers: {
+    setTemperatureUnit: (state, action: PayloadAction<TemperatureUnit>) => {
+      state.temperatureUnit = action.payload;
+    },
 
-        setSpeedUnit: (state, action: PayloadAction<SpeedUnit>) => {
-            state.speedUnit = action.payload
-        }
-    }
-})
+    setSpeedUnit: (state, action: PayloadAction<SpeedUnit>) => {
+      state.speedUnit = action.payload;
+    },
+  },
+});
 
-
-export const {setTemperatureUnit, setSpeedUnit} = weatherSlice.actions;
+export const { setTemperatureUnit, setSpeedUnit } = weatherSlice.actions;
 export default weatherSlice.reducer;
