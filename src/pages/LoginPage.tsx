b94d5c2cfafede870d446c6362b15f46d0f22544
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-11 10:41:47 -0700

    feat: created LoginPage and added the LoginCard

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginCard from "../components/LoginCard.js"
+
+export default function LoginPage(){
+    return(
+        <div className="screen">
+            <LoginCard/>
+        </div>
+    )
+}
