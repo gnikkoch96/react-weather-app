da09fb8ee7e81bbca1c28be7c0d2400e2c834279
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-13 16:40:43 -0700

    feat: enhance LoginCard layout and styling for improved user experience

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -3,35 +3,54 @@ import { Link } from "react-router";
 
 export default function LoginCard() {
   return (
-    <div className="card">
+    <div className="card p-10 gap-1 min-w-100 auto">
       {/* Login Label */}
-      <h2 className="text-2xl font-bold">Login</h2>
+      <h2 className="text-5xl font-bold mb-10">Login</h2>
 
       {/* Login Form */}
-      <div>
+      <div className="min-w-full flex flex-col gap-2">
         {/* E-mail */}
-        <div>
-          <label className="flex items-center gap-1" htmlFor="email-field">
-            <Mail strokeWidth={1.5} size={20}/> E-MAIL ADDRESS
+        <div className="flex flex-col gap-1">
+          <label className="flex items-center gap-1 font-light" htmlFor="email-field">
+            <Mail strokeWidth={1.5} size={20} /> E-MAIL ADDRESS
           </label>
-          <input id='email-field' className="min-w-full p-1 border border-white rounded" type="email" />
+          <input
+            id="email-field"
+            className="min-w-full text-2xl p-2 border border-white rounded"
+            placeholder="name@example.com"
+            type="email"
+          />
         </div>
 
         {/* Password */}
-        <div>
-          <label htmlFor=""><Lock/> PASSWORD </label>
-          <input className="border border-white rounded" type="password" />
+        <div className="min-w-full flex flex-col gap-2">
+          <label className="flex items-center gap-1 font-light" htmlFor="">
+            <Lock strokeWidth={1.5} size={20} /> PASSWORD{" "}
+          </label>
+          <input
+            className="min-w-full text-2xl p-2 border border-white rounded"
+            placeholder="password"
+            type="password"
+          />
         </div>
       </div>
-
+      
       {/* Forgot Password Nav Link */}
-      <Link className='underline hover:opacity-70' to="/">Forgot Password?</Link>
+      <Link className="self-end underline hover:opacity-70 mb-8" to="/">
+        Forgot Password?
+      </Link>
 
       {/* Login Button */}
-      <button className="border">Login</button>
+      <button className="transition duration-200 ease-in cursor-pointer bg-blue-400 min-w-full text-2xl p-2 rounded shadow hover:shadow-[0_0_45px_rgba(34,211,238,1)]">Log In</button>
 
       {/* Sign-up Nav Link */}
-      <p>Don't have an account? <Link to="/" className="underline">Sign up!</Link></p>
+      <p>
+        Don't have an account?{" "}
+        <Link to="/" className="underline hover:opacity-70git ">
+          Sign up!
+        </Link>
+      </p>
+
     </div>
   );
 }
