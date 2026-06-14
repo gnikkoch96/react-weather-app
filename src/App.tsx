dae52900f494e2bf4698f67e170dc25361086ccf
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-14 16:55:39 -0700

    feat: created a new route using wildcard to capture any non existing route and guide the user to the not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import WeatherPage from "./pages/WeatherPage.js";
 import LoginPage from "./pages/LoginPage.js";
+import NotFoundPage from "./pages/NotFoundPage.js";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route index element={<LoginPage/>}/>
         <Route path='/weather' element={<WeatherPage/>}/>
+        <Route path='*' element={<NotFoundPage/>}/>
       </Routes>
     </BrowserRouter>
   );
