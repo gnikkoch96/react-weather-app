a73c02781e025a6eb3b3badac47e0f1d143ac572
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-16 15:26:46 -0700

    feat: store a list of valid credentials

---
+++ src/data/userCredentials.ts
@@ -0,0 +1,16 @@
+export const validUsers = [
+    {
+        username: 'user1',
+        password: 'password'
+    },
+    
+    {
+        username: 'user2',
+        password: 'password2'
+    }, 
+    
+    {
+        username: 'user3',
+        password: 'password3'
+    }
+]
