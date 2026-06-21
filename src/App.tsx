853c754a57f3139f3714c6341b8e30a54020c0bc
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-21 10:25:15 -0700

    feat: created a route to handle protected routes and moved the /weater route within it

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import WeatherPage from "./pages/WeatherPage.js";
 import LoginPage from "./pages/LoginPage.js";
 import NotFoundPage from "./pages/NotFoundPage.js";
+import ProtectedRoute from "./components/ProtectedRoute.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LoginPage/>}/>
-        <Route path='/weather' element={<WeatherPage/>}/>
-        <Route path='*' element={<NotFoundPage/>}/>
+        <Route index element={<LoginPage />} />
+        
+        <Route element={<ProtectedRoute />}>
+          <Route path="weather" element={<WeatherPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />np
       </Routes>
     </BrowserRouter>
   );
