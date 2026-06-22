63fa472e130a6ef4b7650d0ef754d99d54199ab2
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-06-22 16:44:18 -0700

    feat: added a listener to the isLoggedIn global state to check if user is logged in and if so redirect them back to the weather card page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,19 @@
+import { useNavigate } from "react-router";
 import LoginCard from "../components/LoginCard.js"
+import { useAppSelector } from "../hooks/useAppSelector.js"
+import { useEffect } from "react";
 
 export default function LoginPage(){
+    const isLoggedIn = useAppSelector((state) => state.authConfig.isLoggedIn);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(isLoggedIn);
+        if(isLoggedIn){
+            navigate('/weather');
+        }
+    }, [isLoggedIn])
+
     return(
         <main className="screen">
             <LoginCard/>
