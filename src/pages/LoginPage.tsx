037a983851f9861c25814e52ed3fbf042edeba2c
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-07-16 14:28:35 -0700

    chore: added react-hook-form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,18 @@
-import { useNavigate, Navigate} from "react-router";
-import LoginCard from "../components/LoginCard.js"
-import { useAppSelector } from "../hooks/useAppSelector.js"
+import { useNavigate, Navigate } from "react-router";
+import LoginCard from "../components/LoginCard.js";
+import { useAppSelector } from "../hooks/useAppSelector.js";
 
-export default function LoginPage(){
-    const isLoggedIn = useAppSelector((state) => state.authConfig.isLoggedIn);
-    const navigate = useNavigate();
+export default function LoginPage() {
+  const isLoggedIn = useAppSelector((state) => state.authConfig.isLoggedIn);
+  const navigate = useNavigate();
 
-    if(isLoggedIn){
-        return <Navigate to='/weather' replace/>;
-    }
+  if (isLoggedIn) {
+    return <Navigate to="/weather" replace />;
+  }
 
-    return(
-        <main className="screen">
-            <LoginCard onSuccess={() => navigate('/weather')}/>
-        </main>
-    )
+  return (
+    <main className="screen">
+      <LoginCard onSuccess={() => navigate("/weather")} />
+    </main>
+  );
 }
