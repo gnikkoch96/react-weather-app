3413319ff125d5d9afab45f1e6f7dafee26103b4
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-08-05 09:05:15 -0700

    feat: add useLocation hook to manage react state of location service

---
+++ src/hooks/useLocation.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { getLocation } from "../services/location.js";
+import type { LocationData } from "../../types/location/types.js";
+
+export function useLocation(city: string) {
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [locationData, setLocationData] = useState<LocationData[] | null>(null);
+
+  // call service
+  useEffect(() => {
+    const fetchLocationData = async () => {
+      setIsLoading(true);
+
+      try {
+        const data = await getLocation(city);
+        setLocationData(data);
+      } catch (error) {
+        if(error instanceof Error){
+          setError(error.message);
+        }else{
+          setError("An unexpected error occurred")
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchLocationData();
+  }, []);
+
+  return {
+    error,
+    isLoading,
+    locationData,
+  };
+}
