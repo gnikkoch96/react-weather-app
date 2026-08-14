15e3c2febfdfeb114ea155435e21499be8c3a268
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-08-14 10:41:08 -0700

    style: improve search location bar ui

--- src/components/SearchLocationBar.tsx
+++ src/components/SearchLocationBar.tsx
@@ -8,13 +8,18 @@ type Inputs = {
 type SearchLocationBarProps = {
   locationLoading: boolean;
   fetchLocationData: (city: string) => void;
-}
+};
 
 export default function SearchLocationBar({
   locationLoading,
   fetchLocationData,
 }: SearchLocationBarProps) {
-  const { register, handleSubmit, reset, formState:{errors} } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     const { city } = data;
@@ -22,17 +27,21 @@ export default function SearchLocationBar({
   };
 
   return (
-    <form className='relative' onSubmit={handleSubmit(onSubmit)}>
+    <form className="relative" onSubmit={handleSubmit(onSubmit)}>
       <input
         type="text"
         placeholder="Enter City"
         className="bg-white border rounded-2xl p-1.5 pr-9 shadow"
-        {...register("city", { required: 'Please enter a city' })}
+        {...register("city", { required: "Please enter a city" })}
       />
-      <button className='absolute p-0.5 rounded-2xl bg-primary text-white right-1.5 top-2 hover:cursor-pointer transition duration-100 ease-in ' disabled={locationLoading} type="submit">
-        <Search size={20}/>
+      <button
+        className="absolute p-0.5 rounded-2xl bg-primary text-white right-1.5 top-2 hover:cursor-pointer transition duration-100 ease-in "
+        disabled={locationLoading}
+        type="submit"
+      >
+        <Search size={20} />
       </button>
-      {errors.city && (<p>{errors.city.message}</p>)}
+      {errors.city && <p className='text-white ml-1.5'>{errors.city.message}</p>}
     </form>
   );
 }
