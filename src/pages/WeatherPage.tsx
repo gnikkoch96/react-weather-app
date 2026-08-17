9cb39012f37ea76c83b16a79342ad5eb95a37f65
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-08-17 09:35:50 -0700

    fix: refetch weather data upon changing weather settings

--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -50,7 +50,7 @@ export default function WeatherPage() {
       temperatureUnit,
       speedUnit,
     );
-  }, [selectedLocation]);
+  }, [selectedLocation, temperatureUnit, speedUnit]);
 
   const handleLocationSelect = (location: LocationData) => {
     setSelectedLocation(location);
