7110282cdd8393e754293087b0b7ef721481d511
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-09-04 13:58:03 -0700

    chore(abort): add unit tests

--- src/tests/abort.test.ts
+++ src/tests/abort.test.ts
@@ -1,5 +1,31 @@
+import { jest } from "@jest/globals";
 import { createAbortSignal } from "../utils/abort.js";
 
-test('create abort signals', () => {
+beforeEach(() => {
+    jest.useFakeTimers();
+});
 
+afterEach(() => {
+    jest.useRealTimers();
+});
+
+test("returns AbortSignal", () => {
+  const { signal } = createAbortSignal(undefined, 1000);
+  expect(signal).toBeInstanceOf(AbortSignal);
+  expect(signal.aborted).toBe(false);
+});
+
+test("aborts signal after timeout", () => {
+  const { signal } = createAbortSignal(undefined, 1000);
+  expect(signal.aborted).toBe(false);
+
+  jest.advanceTimersByTime(1001);
+  expect(signal.aborted).toBe(true);
+});
+
+test("prevents signal from aborting after timeout cleanup", () => {
+    const {signal, timeoutCleanup} = createAbortSignal(undefined, 1000);
+    expect(signal.aborted).toBe(false);
+    timeoutCleanup();
+    expect(signal.aborted).toBe(false);
 });
