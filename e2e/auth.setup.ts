e337f1e446737d4ebc1a3395616bea619acb2613
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-09-11 14:29:13 -0700

    fix(playwright): log in each time a test is ran

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -4,19 +4,17 @@ setup("authenticate", async ({ page }) => {
   // go to login page
   await page.goto("http://localhost:5173/weather");
 
-  if (page.url() === "http://localhost:5173/") {
-    // find email input and fill in data
-    await page.getByLabel("E-MAIL ADDRESS").fill("user1@email.com");
+  // find email input and fill in data
+  await page.getByLabel("E-MAIL ADDRESS").fill("user1@email.com");
 
-    // find password input and fill in data
-    await page.getByLabel("PASSWORD").fill("password");
+  // find password input and fill in data
+  await page.getByLabel("PASSWORD").fill("password");
 
-    // find and click on the login button
-    await page.getByRole("button", { name: "LOG IN" }).click();
+  // find and click on the login button
+  await page.getByRole("button", { name: "LOG IN" }).click();
 
-    // verify that login succeeded
-    await expect(page).toHaveURL("http://localhost:5173/weather");
-  }
+  // verify that login succeeded
+  await expect(page).toHaveURL("http://localhost:5173/weather");
 
   // save the logged-in browser state
   await page.context().storageState({
