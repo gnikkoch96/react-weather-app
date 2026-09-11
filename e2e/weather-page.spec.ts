e6f258659a3009b2447f489df6a10a1b932452cf
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-09-11 15:54:23 -0700

    test(weather): add e2e test for searching and viewing weather

--- e2e/weather-page.spec.ts
+++ e2e/weather-page.spec.ts
@@ -1,5 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('search location and view weather', async({page}) => {
-    await page.goto('http://localhost:5173/weather');
+test("search location and view weather", async ({ page }) => {
+  await page.goto("http://localhost:5173/weather");
+
+  await page.getByLabel("Enter City:").fill("Los Angeles");
+
+  await page.getByRole("button", { name: "Search Location" }).click();
+
+  await expect(
+    page.getByRole("dialog", { name: "Select Location" }),
+  ).toBeVisible();
+
+  await page
+    .getByRole("dialog", { name: "Select Location" })
+    .getByRole("list")
+    .getByRole("button")
+    .first()
+    .click();
+
+  await expect(page.getByText('Relative Humidity')).toBeVisible();
 });
